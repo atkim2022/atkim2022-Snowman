daee52947b70eb7171110d375bf1d257df8293b7
atkim2022 <atkim2022@chadwickschool.org>
2021-11-17 14:55:48 -0800

    milestone 5

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -6,18 +6,22 @@ public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
     public UnityEngine.UI.Button StartButton;
+    public UnityEngine.UI.Button BackButton;
     public GameObject StartScreen;
     public GameObject PlayScreen;  
     public void StartGame() 
     {
     this.Message.text = "Can you save the snowman?";
     this.StartButton.gameObject.SetActive(false);
+    this.BackButton.gameObject.SetActive(true);
     this.StartScreen.gameObject.SetActive(false);
     this.PlayScreen.gameObject.SetActive(true);
     } 
  
     public void OpenStartScreen()
     {
+    this.StartButton.gameObject.SetActive(true);
+    this.BackButton.gameObject.SetActive(false);
     this.StartScreen.gameObject.SetActive(true);
     this.PlayScreen.gameObject.SetActive(false);
     }
