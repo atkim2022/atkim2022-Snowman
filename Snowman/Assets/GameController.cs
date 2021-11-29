ca0cd3b3bb1e2c20422e9a3df00447f27c6e269f
atkim2022 <atkim2022@chadwickschool.org>
2021-11-29 10:17:18 -0800

    5

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -12,6 +12,13 @@ public class GameController : MonoBehaviour
     public GameObject PlayScreen;
     private WordGame guessingGame;
     public UnityEngine.UI.InputField PlayerGuess;
+    public UnityEngine.UI.Text HiddenLetters; 
+    public UnityEngine.UI.Text LettersGuessed; 
+    public UnityEngine.UI.Text GuessesRemaining;
+    public UnityEngine.UI.Text Guess;  
+    public UnityEngine.UI.Text AfterGuess;  
+
+
     public void StartGame()
     {
         this.guessingGame = new WordGame("apple", 5);
@@ -37,6 +44,11 @@ public class GameController : MonoBehaviour
     public void SubmitGuess()
     {
         Debug.Log(this.guessingGame.CheckGuess(PlayerGuess.text));
+        Guess.text = PlayerGuess.text;
+        HiddenLetters.text = this.guessingGame.GetWord();
+        LettersGuessed.text = this.guessingGame.GetGuessedLetters();
+        AfterGuess.text = this.guessingGame.CheckGuess(PlayerGuess.text);
+        GuessesRemaining.text = $"{this.guessingGame.GetGuessLimit() - this.guessingGame.GetIncorrectGuesses()}";
         PlayerGuess.text = string.Empty;
     }
 }
